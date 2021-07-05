d31ef3caf4328714c6cd9f3bf7477310cd39f562
Mauro <Maurovera069@gmail.com>
2021-07-05 11:09:18 -0300

    Defini las tablas

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -2,11 +2,48 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
+  // Tabla de Pokemon
   sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      vida: {
+        type: DataTypes.INTEGER
+      },
+      fuerza: {
+        type: DataTypes.INTEGER
+      },
+      defensa: {
+        type: DataTypes.INTEGER 
+      },
+      Velocidad: {
+        type: DataTypes.INTEGER
+      },
+      Altura: {
+        type: DataTypes.INTEGER
+      },
+      Peso: {
+        type: DataTypes.INTEGER
+      }
+  }, {
+    timestamps: false
   });
+
+  // Tabla de Pokemon_Type
+  sequelize.define('pokemon_types', {
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+  }, {
+    timestamps: false
+  });
+
+
+
+
+/* --------------------------------- */
 };
+
