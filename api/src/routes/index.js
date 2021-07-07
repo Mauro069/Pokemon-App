0993b16f6b0121bc1da17c621fe97bddf0792347
Mauro <Maurovera069@gmail.com>
2021-07-07 11:31:50 -0300

    Empezamos devuelta

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,10 @@
 const { Router } = require('express');
-const pokemons =  require('./pokemon') 
 const types =  require('./type') 
 
 const router = Router();
 
 // Middlewares
-router.use('/pokemons', pokemons)
-router.use('/types', types)
+router.use('/pokemons',)
+// router.use('/types', types)
 
 module.exports = router
