24c3dd7844adeb5ce153e644874c8270a1cbcc0b
Mauro <Maurovera069@gmail.com>
2021-07-12 11:39:04 -0300

    Nav ready

--- client/src/controllers/getPokemonsViews.js
+++
@@ -1,9 +0,0 @@
-const GetPokemonsViews = (array, min, max) => {
-  if (!min && !max) {
-    console.log(array);
-    return array.slice(0, 12);
-  }
-  return array.slice(min, max);
-};
-
-export default GetPokemonsViews;
--- client/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import Reducer from "../reducer/index";
-import thunk from "redux-thunk";
-
-const store = createStore(Reducer, compose(applyMiddleware(thunk)));
-
-export default store;
